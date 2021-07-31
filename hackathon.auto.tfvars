37181b1c42e1e0e310f89f755d2ae8a62b43f0a0
Wei Zixi <wellsgz@gmail.com>
2021-07-31 17:55:00 +0800

    Commit

--- hackathon.auto.tfvars
+++
@@ -1,4 +0,0 @@
-resource "heroku_app" "example" {
-  name   = var.example_app_name
-  region = "us"
-}
--- main.tf
+++ main.tf
@@ -9,3 +9,7 @@ terraform {
 variable "example_app_name" {
   description = "Name of the Heroku app provisioned as an example"
 }
+resource "heroku_app" "example" {
+  name   = var.example_app_name
+  region = "us"
+}
